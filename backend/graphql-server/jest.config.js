b563e04b8521afc2c5ef48926df43e3810ce9e29
Julia Groza <jgroza24@gmail.com>
2026-01-01 23:23:35 -0500

    Replace deprecated ts-jest usage

--- backend/graphql-server/jest.config.js
+++ backend/graphql-server/jest.config.js
@@ -1,16 +1,7 @@
-const { createDefaultPreset } = require('ts-jest');
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    ...tsJestTransformCfg,
+    '^.+\\.(t|j)sx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json'
-    }
-  }
 };
