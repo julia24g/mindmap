c06d9f63c23f065c2a2dd71d6a3ce48dffb9ee48
Julia Groza <jgroza24@gmail.com>
2026-01-01 23:02:35 -0500

    Fixed github workflow errors

---
+++ database/postgres-init.sql
@@ -0,0 +1,20 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS contents (
+  contentid uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  userid uuid NOT NULL,
+  title text NOT NULL,
+  type text,
+  created_at timestamptz NOT NULL DEFAULT now(),
+  properties jsonb
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  userid uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  firstname text NOT NULL,
+  lastname text NOT NULL,
+  email text NOT NULL UNIQUE,
+  passwordhash text NOT NULL,
+  createdat timestamptz NOT NULL DEFAULT now(),
+  updatedat timestamptz
+);
