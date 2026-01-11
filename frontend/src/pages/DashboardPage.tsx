f5964c06e0c57bfdd970ff2361360f29cc1d32d2
Julia Groza <jgroza24@gmail.com>
2026-01-10 22:28:13 -0500

    Initial implementation of React Flow

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,11 +1,41 @@
+import { useState } from 'react';
+import { 
+  ReactFlow, 
+  Controls,
+  Background,
+  Node,
+  Edge } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+
 import AddContent from "@/components/AddContent";
 import { TypographyH1 } from "@/typography/TypographyH1";
 
+const initialNodes: Node[] = [
+  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+];
+const initialEdges: Edge[] = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+
 export default function DashboardPage() {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+  const [variant, setVariant] = useState('dots');
+
+
   return (
     <>
         <TypographyH1>Dashboard</TypographyH1>
         <AddContent />
+        <div style={{ width: '100vw', height: '100vh' }}>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            fitView
+          >
+            <Background variant={variant} />
+            <Controls showInteractive={false} />
+          </ReactFlow>
+        </div>
     </>
   )
 }
